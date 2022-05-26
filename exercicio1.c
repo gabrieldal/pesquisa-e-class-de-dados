aa07a47ee4ea55ef1886f8b7a7696f0670ca9f31
Gabriel Dalmazo <gabrieldambros.aluno@unipampa.edu.br>
2022-05-26 10:42:36 -0300

    Create exercicio1.c
    para ser feito

---
+++ exercicio1.c
@@ -0,0 +1,26 @@
+/*
+a) Faça um programa que receba um vetor A, a posição de início "ini" e final "fim".
+- ordene esse valor com bubble sort.
+
+b) faça uma função que receba um vetor A(inteiro), a posição de início, de final e divisão das duas partes).
+- Cada metade do vetor já foi previamente ordenada.
+- Copie os elementos para outro vetor B, de forma que B fique totalmente ordenado.
+c) Faça um programa que:
+- prencha o vetor A aleatoriamente;
+- calcule o meio;
+- ordene cada metade;(duas chamdas de bubble)
+- junte tudo;
+- mostre o resultado;
+*/ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define TAM 30
+
+
+int main()
+{
+    
+    return 0;
+}
